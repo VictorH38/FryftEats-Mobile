f732ae656e2d1e5e08ba64f5cbb7b8e13b39eede
Victor Hoang <victorho@usc.edu>
2024-04-28 13:52:29 -0700

    Initial Commit

---
+++ FryftEats/FryftEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FryftEatsApp.swift
+//  FryftEats
+//
+//  Created by Victor Hoang on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FryftEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
